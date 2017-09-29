2d2d7683ca1c4e53d715d957a706d2fa55fe8c8f
David Ding <chengjie.ding@gmail.com>
2017-09-29 10:28:02 -0500

    solution of poj 1001 (#2)
    Solution of poj 1001

--- poj/exponentiation.c
+++ poj/exponentiation.c
@@ -2,19 +2,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define SIZE 10
-#define MAX  1024
+#define SIZE 20
+#define MAX  1000
 
-char *exponent(char *s, int n);
+int exponent(char *s, int n, char *buf, const int size);
 
 int main(void)
 {
     char s[SIZE];
-    int n;
+    int n, r;
 
     while(scanf("%s%d", s, &n) == 2)
     {
-        char *e = exponent(s, n);
+        char *e = (char *) malloc(sizeof(char) * MAX);
+        r = exponent(s, n, e, MAX);
+        if(r) continue;
         printf("%s\n", e);
         free(e);
     }
@@ -22,11 +24,81 @@ int main(void)
     return 0;
 }
 
-char *exponent(char *s, int n)
+/**
+ * Compute exponentiation.
+ *
+ *   str: base in string
+ *     n: index number
+ *     r: char array contains return value
+ *  size: size of the char array
+ */
+int exponent(char *str, int n, char *r, const int size)
 {
-    char *r = (char *)malloc(100);
+    int i, num, s, c;
+    int dotpos = 0, nLen = 0;
+    char buf[MAX];
 
-    strcpy(r, "hello");
+    memset(buf, 0, size * sizeof(char));
+    if(size <= SIZE)
+    {
+        return -1;
+    }
 
-    return r;
+    if(n == 0)
+    {
+        strcpy(r, "1.0");
+        return 0;
+    }
+
+    // omit '.' in string 'str'.
+    nLen = strlen(str);
+    i = 0;
+    while(str[i] != '.')
+        i++;
+    dotpos = nLen - i - 1;
+    for(; i < nLen; i++)
+        str[i] = str[i+1];
+    // convert real number to integer.
+    num = atoi(str);
+    // find the dot position.
+    dotpos = dotpos * n;
+    // reset response buffer.
+    memset(r, 0, size * sizeof(char));
+    // start compute, result in reverse sequence.
+    buf[1] = 1;
+    while(n > 0)
+    {
+        s = 0, c = 0;
+        for(i = 0; i < size; i++)
+        {
+            s = num * buf[i] + c;
+            c = s / 10;
+            buf[i] = s % 10;
+        }
+        --n;
+    }
+    // heading '.' checking
+    for(i = size - 1; buf[i] == 0; --i);
+    i = (i < dotpos) ? dotpos : i;
+    while(i > 0)
+    {
+        char n[2];
+        if(i == dotpos) strcat(r, ".");
+        sprintf(n, "%d", buf[i--]);
+        strcat(r, n);
+    }
+    // omit tailing '0'
+    nLen = strlen(r);
+    for(i = nLen - 1; i > 0; --i)
+        if(r[i] == '0')
+        {
+            r[i] = 0;
+        }
+        else
+        {
+            break;
+        }
+    if(r[i] == '.') r[i] = 0;
+
+    return 0;
 }
