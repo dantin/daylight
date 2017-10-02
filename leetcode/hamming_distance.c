56222562531778ed0651f17787ad3a4d662c6a42
David Ding <chengjie.ding@gmail.com>
2017-10-02 10:33:17 -0500

    hamming_distance from leetcode. (#4)

---
+++ leetcode/hamming_distance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int hammingDistance(int x, int y)
+{
+    int d = 0, xor = x ^ y;
+
+    while(xor != 0)
+    {
+        ++d;
+        xor &= xor - 1;
+    }
+
+    return d;
+}
