dd45025cea107215e759fc2f687f5708e9507e30
David Ding <chengjie.ding@gmail.com>
2018-01-03 00:13:00 +0800

    sulution of reverse integer in cpp. (#9)

---
+++ leetcode/reverse-integer/reverse_integer.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int reverse(int x) {
+    int ans = 0;
+    while (x) {
+      int t = ans * 10 + x % 10;
+      if (t/10 != ans) {
+        return 0;
+      }
+      ans = t;
+      x /= 10;
+    }
+    return ans;
+  }
+};
+
+int main() {
+  int num;
+  cin >> num;
+  Solution s;
+  cout << s.reverse(num) << endl;
+  return 0;
+}
