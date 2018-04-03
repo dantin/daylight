ed7777acf7b7438915a4071a1028cbd88a98d416
dantin <chengjie.ding@gmail.com>
2018-04-03 23:15:33 +0800

    refactoring solution of two sum.

--- leetcode/two-sum/two_sum.cc
+++ leetcode/two-sum/two_sum.cc
@@ -1,23 +1,30 @@
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <iostream>
 
 using namespace std;
 
 class Solution {
 public:
-  vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> h;
+  vector<int> twoSum(vector<int>& numbers, int target) {
+    map<int, int> mapping;
     vector<int> result;
-    for (int i = 0; i < nums.size(); i++) {
-      int remains = target - nums[i];
-      if (h.find(remains) != h.end()) {
-        result.push_back(h[remains]);
-        result.push_back(i);
-        return result;
+    for (int i = 0; i < numbers.size(); i++)
+      {
+        mapping[numbers[i]] = i;
       }
-      h[nums[i]] = i;
-    }
+
+    for (int i = 0; i < numbers.size(); i++)
+      {
+        int searched = target - numbers[i];
+        if (mapping.find(searched) != mapping.end() &&
+            i != mapping[searched])
+          {
+            result.push_back(i+1);
+            result.push_back(mapping[searched]+1);
+            break;
+          }
+      }
     return result;
   }
 };
