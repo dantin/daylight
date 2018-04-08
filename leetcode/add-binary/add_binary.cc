b8334c7d2e681fdeb955ab20c9cca7703d141b82
dantin <chengjie.ding@gmail.com>
2018-04-08 15:24:37 +0800

    solution of add binary.

---
+++ leetcode/add-binary/add_binary.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  string addBinary(string a, string b)
+  {
+    int carry = 0;
+    string result;
+    for (int i = a.size()-1, j = b.size()-1; i >= 0 || j >= 0; --i, --j) {
+      int ai = i >= 0 ? a[i] - '0' : 0;
+      int bj = j >= 0 ? b[j] - '0' : 0;
+      int val = (ai + bj + carry) % 2;
+      carry = (ai + bj + carry) / 2;
+      result.insert(result.begin(), val + '0');
+    }
+    if (carry == 1) {
+      result.insert(result.begin(), '1');
+    }
+    return result;
+  }
+};
+
+int main()
+{
+  string a, b;
+  cin >> a >> b;
+  Solution s;
+  string result = s.addBinary(a, b);
+  cout << result << endl;
+
+  return 0;
+}
