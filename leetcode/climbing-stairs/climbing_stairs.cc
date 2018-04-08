d57cec90ed21566c743774eec94225576f516048
dantin <chengjie.ding@gmail.com>
2018-04-08 17:24:40 +0800

    solution of climbing stairs.

---
+++ leetcode/climbing-stairs/climbing_stairs.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+  int climbStairs(int n) {
+    int fn_2 = 1, fn_1 = 2;
+    if (n == 1) return fn_2;
+    if (n == 2) return fn_1;
+    int fn;
+    for (int i = 3; i <= n; ++i)
+      {
+        fn = fn_2 + fn_1;
+        fn_2 = fn_1;
+        fn_1 = fn;
+      }
+    return fn;
+  }
+};
+
+int main()
+{
+  int n;
+
+  cin >> n;
+
+  Solution s;
+  cout << s.climbStairs(n) << endl;
+
+  return 0;
+}
