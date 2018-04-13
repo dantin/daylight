36f6366870fbc522d3d990303b5e4171416dc315
dantin <chengjie.ding@gmail.com>
2018-04-13 11:31:09 +0800

    solution of count and say.

---
+++ leetcode/count-and-say/count_and_say.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+  string countAndSay(int n)
+  {
+    string seq = "1";
+    int it = 1;
+    while (it < n) {
+      stringstream newSeq;
+      char last = seq[0];
+      int count = 0;
+      for (int i = 0; i <= seq.size(); ++i) {
+        if (seq[i] == last) {
+          count++;
+          continue;
+        } else {
+          newSeq << count << last;
+          last = seq[i];
+          count = 1;
+        }
+      }
+      seq = newSeq.str();
+      it++;
+    }
+    return seq;
+  }
+};
+
+int main()
+{
+  int n;
+
+  cin >> n;
+
+  Solution s;
+  cout << s.countAndSay(n) << endl;
+
+  return 0;
+}
