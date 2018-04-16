a6bd92e7149cd0e343c837b57c17b99c8efe1d52
dantin <chengjie.ding@gmail.com>
2018-04-16 15:59:22 +0800

    solution of evaluate reverse polish notation.

---
+++ leetcode/evaluate-reverse-polish-notation/eval_rpn.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int evalRPN(vector<string> &tokens)
+  {
+    stack<int> operand;
+    for (int i = 0; i < tokens.size(); ++i) {
+      if ((tokens[i][0] == '-' && tokens[i].size() > 1) ||
+          (tokens[i][0] >= '0' && tokens[i][0] <= '9')) {
+        operand.push(atoi(tokens[i].c_str()));
+        continue;
+      }
+      int op1 = operand.top();
+      operand.pop();
+      int op2 = operand.top();
+      operand.pop();
+      if (tokens[i] == "+") operand.push(op2+op1);
+      if (tokens[i] == "-") operand.push(op2-op1);
+      if (tokens[i] == "*") operand.push(op2*op1);
+      if (tokens[i] == "/") operand.push(op2/op1);
+    }
+    return operand.top();
+  }
+};
+
+int main()
+{
+  vector<string> tokens;
+  string token;
+
+  while (cin >> token) {
+    tokens.push_back(token);
+  }
+
+  Solution s;
+  cout << s.evalRPN(tokens) << endl;
+  
+  return 0;
+}
