b5a7b5432e8f0eaf05112052655c56a74dcbeed7
dantin <chengjie.ding@gmail.com>
2018-05-28 15:20:36 +0800

    solution of letter combinations of a phone number.

---
+++ leetcode/letter-combinations-of-a-phone-number/letter_combinations.cc
@@ -0,0 +1,53 @@
+// version 1.0 2018-05-28
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits)
+    {
+      string trans[] = { "", " ", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+      vector<string> set;
+      string seq;
+
+      generate(trans, digits, 0, seq, set);
+
+      return set;
+    }
+
+private:
+    void generate(string trans[], string &digits, int deep, string &result, vector<string> &set)
+    {
+      if (deep == digits.size()) {
+        set.push_back(result);
+        return;
+      }
+
+      // convert ASCII code to integer
+      int curDig = digits[deep] - 48;
+      for (int i = 0; i < trans[curDig].size(); ++i) {
+        result.push_back(trans[curDig][i]);
+        generate(trans, digits, deep+1, result, set);
+        result.resize(result.size()-1);
+      }
+    }
+};
+
+int main()
+{
+  string digits;
+
+  cin >> digits;
+
+  Solution s;
+  auto combs = s.letterCombinations(digits);
+
+  for (auto comb : combs) {
+    cout << comb << endl;
+  }
+  cout << endl;
+
+  return 0;
+}
