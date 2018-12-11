f98e1a1c7a972f5db92d5f4a4a79edd9ce0a60f9
David Ding <chengjie.ding@gmail.com>
2018-12-11 10:45:39 +0800

    c solution of add two number. (#13)
    * c solution of add two number.
    
    * update Makefile.

---
+++ leetcode/002-add-two-numbers/add_two_numbers.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Definition for single-linked list. */
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+static struct ListNode* add_two_numbers(struct ListNode* l1, struct ListNode* l2)
+{
+    int carry = 0;
+    int first = 1;
+    struct ListNode *res = NULL;
+    struct ListNode *p = NULL;
+    struct ListNode *prev = p;
+
+    while (l1 != NULL || l2 != NULL || carry) {
+        int sum = 0;
+        int last_carry = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        if (sum >= 10) {
+            sum -= 10;
+            carry = 1;
+        } else {
+            carry = 0;
+        }
+
+        p = malloc(sizeof(*p));
+        if (first) {
+            res = p;
+            first = 0;
+        }
+        p->val = sum + last_carry;
+        if (p->val >= 10) {
+            p->val -= 10;
+            carry = 1;
+        }
+        p->next = NULL;
+        if (prev != NULL) {
+            prev->next = p;
+        }
+        prev = p;
+    }
+
+    return res;
+}
+
+static struct ListNode *build(const char *digits)
+{
+    struct ListNode *res, *p, *prev;
+    int first = 1;
+    int len = strlen(digits);
+    const char *c = digits + len - 1;
+
+    prev = NULL;
+    while (len-- > 0) {
+        p = malloc(sizeof(*p));
+        if (first) {
+            first = 0;
+            res = p;
+        }
+        p->val = *c-- - '0';
+        p->next = NULL;
+        if (prev != NULL) {
+            prev->next = p;
+        }
+        prev = p;
+    }
+
+    return res;
+}
+
+static void show(struct ListNode *ln)
+{
+    int sum = 0, factor = 1;
+
+    while (ln != NULL) {
+        sum += ln->val * factor;
+        factor *= 10;
+        ln = ln->next;
+    }
+    printf("%d\n", sum);
+}
+
+int main(int argc, const char *argv[])
+{
+    if (argc < 3) {
+        fprintf(stderr, "Usage: ./dist n1 n2\n");
+        exit(-1);
+    }
+
+    struct ListNode *l1 = build(argv[1]);
+    struct ListNode *l2 = build(argv[2]);
+    struct ListNode *res = add_two_numbers(l1, l2);
+
+    show(l1);
+    show(l2);
+    show(res);
+
+    return 0;
+}
