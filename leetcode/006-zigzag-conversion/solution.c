c041bd5abcf8507f37423036ab6041646f57136e
dantin <chengjie.ding@gmail.com>
2018-12-18 14:05:10 +0800

    add solution.

--- leetcode/006-zigzag-conversion/solution.c
+++ leetcode/006-zigzag-conversion/solution.c
@@ -7,6 +7,35 @@ struct object {
     int rows;
 };
 
+char *convert(char *s, int rows)
+{
+    if (rows <= 1) return s;
+
+    int len = strlen(s);
+    char *str = malloc(len+1);
+    char *p = str;
+    int row;
+
+    for (row = 0; row < rows; row++) {
+        int interval1 = rows + (rows-2) - row*2;
+        int interval2 = row * 2;
+        int flag = 0;
+        int i = row;
+        while (i < len) {
+            *p++ = s[i];
+            int delta = 0;
+            do {
+                delta = (flag == 0) ? interval1 : interval2;
+                flag = !flag;
+            } while (delta == 0);
+            i += delta;
+        }
+    }
+    str[len] = '\0';
+
+    return str;
+}
+
 int main(int argc, char **argv)
 {
     int i;
@@ -14,9 +43,10 @@ int main(int argc, char **argv)
         {"PAYPALISHIRING", 3},
         {"PAYPALISHIRING", 4},
     };
+    int len = sizeof(arr) / sizeof(struct object);
 
-    for (i = 0; i < (sizeof(arr) /sizeof(struct object)) ; i++) {
-        printf("%s, %d\n", arr[i].s, arr[i].rows);
+    for (i = 0; i < len; i++) {
+        printf("%s\n", convert(arr[i].s, arr[i].rows));
     }
 
     return 0;
