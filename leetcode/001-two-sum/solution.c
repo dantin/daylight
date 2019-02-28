10f72d3371ca3ff68dea02f571b80726762008ba
dantin <chengjie.ding@gmail.com>
2019-02-28 16:23:22 +0800

    improvement.

--- leetcode/001-two-sum/solution.c
+++ leetcode/001-two-sum/solution.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Object {
+    int *nums;
+    int len;
+    int target;
+};
+
 struct object {
     int val;
     int index;
 };
 
-void print_array(int *array, int len)
+void print_nums(int *nums, int size)
 {
+    if (size < 1) {
+        printf("[]");
+        return;
+    }
     int i;
 
-    putchar('[');
-    for (i = 0; i < len; i++) {
-        if (i == 0) {
-            printf("%d", array[i]);
-        } else {
-            printf(", %d", array[i]);
-        }
+    printf("[%d", nums[0]);
+    for (i = 1; i < size; i++) {
+        printf(", %d", nums[i]);
     }
-    putchar(']');
+    printf("]");
 }
 
-static int compare(const void *lhs, const void *rhs)
+int compare(const void *lhs, const void *rhs)
 {
     return ((struct object *) lhs)->val - ((struct object *) rhs)->val;
 }
 
-static int * two_sum(int *nums, int numsSize, int target)
+int *two_sum(int *nums, int nums_size, int target)
 {
     int i, j;
-    struct object *objs = malloc(numsSize * sizeof(*objs));
-    for (i = 0; i < numsSize; i++) {
+    struct object *objs = malloc(nums_size * sizeof(*objs));
+    for (i = 0; i < nums_size; i++) {
         objs[i].val = nums[i];
         objs[i].index = i;
     }
-    qsort(objs, numsSize, sizeof(*objs), compare);
+    qsort(objs, nums_size, sizeof(*objs), compare);
 
     int *results = malloc(2 * sizeof(int));
     i = 0;
-    j = numsSize -1;
+    j = nums_size - 1;
     while (i < j) {
         int diff = target - objs[i].val;
         if (diff > objs[j].val) {
@@ -48,27 +54,40 @@ static int * two_sum(int *nums, int numsSize, int target)
         } else {
             results[0] = objs[i].index;
             results[1] = objs[j].index;
+            free(objs);
             return results;
         }
     }
+    free(objs);
+
     return NULL;
 }
 
 int main(int argc, char **argv)
 {
-    int nums[] = { 2, 7, 11, 15 };
-    int len = sizeof(nums) /sizeof(*nums);
-    int target = 9;
+    int nums1[] = { 2, 7, 11, 15 }, len1 = sizeof(nums1) /sizeof(int);
+    int target1 = 9;
+    struct Object inputs[] = {
+        { .nums = nums1, .len = len1, .target = target1 },
+    };
+    int i, len = sizeof(inputs) / sizeof(struct Object);
 
+    for (i = 0; i < len; i++) {
+        int *nums = inputs[i].nums;
+        int size = inputs[i].len;
+        int target = inputs[i].target;
 
-    printf(" Given nums: "); print_array(nums, len);
-    printf(", target = %d\n", target);
+        printf(" Given nums: ");
+        print_nums(nums, size);
+        printf(", target = %d\n", target);
 
-    int *indexes = two_sum(nums, len, target);
-    if (indexes != NULL) {
-        printf(" [%d, %d]\n", indexes[0], indexes[1]);
-    } else {
-        printf(" Not found\n");
+        int *indexes = two_sum(nums, size, target);
+        if (indexes != NULL) {
+            printf(" [%d, %d]\n", indexes[0], indexes[1]);
+        } else {
+            printf(" Not found\n");
+        }
+        free(indexes);
     }
 
     return EXIT_SUCCESS;
