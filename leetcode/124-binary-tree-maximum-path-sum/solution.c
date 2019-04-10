d2d2517384abe5b903651bce42360e91ebdf2f19
dantin <chengjie.ding@gmail.com>
2019-04-10 11:33:29 +0800

    adjust directory structure.

---
+++ leetcode/124-binary-tree-maximum-path-sum/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
