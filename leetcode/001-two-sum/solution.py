75cf63ed28f7af2a8bc65978148bc1fbc01d3a10
David Ding <chengjie.ding@gmail.com>
2019-04-11 14:04:06 +0800

    add new solution. (#157)
    * add new solution of two sum.
    
    * rename.

--- leetcode/001-two-sum/solution.py
+++ leetcode/001-two-sum/solution.py
@@ -11,6 +11,23 @@ class Solution:
             cache[target - num] = i
         return []
 
+    def twoSum2(self, nums: list, target: int) -> list:
+        idx, value = 0, 1
+        objs = [(i, num) for i, num in enumerate(nums)]
+        objs.sort(key=lambda x: x[value])
+        low, high = 0, len(nums) - 1
+        while low < high:
+            diff = target - objs[low][value]
+            if diff > objs[high][value]:
+                while low + 1 < len(nums) and objs[low][value] == objs[low + 1][value]: low += 1
+                low += 1
+            elif diff < objs[high][value]:
+                while high - 1 >= 0 and objs[high][value] == objs[high - 1][value]: high -= 1
+                high -= 1
+            else:
+                return [objs[low][idx], objs[high][idx]]
+        return []
+
 
 if __name__ == '__main__':
     inputs = [
@@ -21,4 +38,6 @@ if __name__ == '__main__':
         print(' Given nums: ', nums, ', target = ', target, sep='')
         solution = Solution()
         pos = solution.twoSum(nums, target)
+        print(' Output: ', pos, sep='')
+        pos = solution.twoSum2(nums, target)
         print(' Output: ', pos, sep='')
