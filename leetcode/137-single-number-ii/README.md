97601ba594b10d733c96bd1c5b1fe5a136f6c8d8
David Ding <chengjie.ding@gmail.com>
2019-04-26 11:31:54 +0800

    solution of single number ii. (#186)

---
+++ leetcode/137-single-number-ii/README.md
@@ -0,0 +1,22 @@
+Single Number II
+---
+
+Given a __non-empty__ array of integers, every element appears _three_ times except for one, which appears exactly once. Find that single one.
+
+__Note__:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+__Example 1__:
+
+```
+Input: [2,2,3,2]
+Output: 3
+```
+
+__Example 2__:
+
+```
+Input: [0,1,0,1,0,1,99]
+Output: 99
+```
---
+++ leetcode/137-single-number-ii/solution.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Object {
+    int *nums;
+    int len;
+};
+
+void print_nums(int *nums, int size)
+{
+    int i;
+    char *s = "";
+    printf("[");
+    for (i = 0; i < size; i++) {
+        printf("%s%d", s, nums[i]);
+        s = ", ";
+    }
+    printf("]");
+}
+
+int single_number(int *nums, int nums_size)
+{
+    int i, j, count[32], mask = 0;
+
+    for (i = 0; i < 32; i++) {
+        count[i] = 0;
+        for (j = 0; j < nums_size; j++) {
+            if ((1 << i) & nums[j]) {
+                count[i]++;
+            }
+        }
+        mask |= (count[i] % 3) << i;
+    }
+
+    return mask;
+}
+
+int main(int argc, char **argv)
+{
+    int nums1[] = { 2, 2, 3, 2 }, len1 = sizeof(nums1) / sizeof(int);
+    int nums2[] = { 0, 1, 0, 1, 0, 1, 99 }, len2 = sizeof(nums2) / sizeof(int);
+    struct Object inputs[] = {
+        { .nums = nums1, .len = len1 },
+        { .nums = nums2, .len = len2 },
+    };
+    int i, len = sizeof(inputs) / sizeof(struct Object);
+
+    for (i = 0; i < len; i++) {
+        int *nums = inputs[i].nums;
+        int size = inputs[i].len;
+
+        printf("\n Input: ");
+        print_nums(nums, size);
+        int num = single_number(nums, size);
+        printf("\n Output: %d\n", num);
+    }
+
+    return EXIT_SUCCESS;
+}
