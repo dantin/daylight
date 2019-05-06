0cd14b9e22722b77a0d1cc5ab7643f65c06eb997
dantin <chengjie.ding@gmail.com>
2019-05-06 15:41:02 +0800

    solution of word break ii.

---
+++ leetcode/140-word-break-ii/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define container_of(ptr, type, member) \
+    ((type *)((char *)(ptr) - (size_t)&(((type *)0)->member)))
+
+#define list_entry(ptr, type, member) \
+    container_of(ptr, type, member)
+
+#define list_for_each(p, head) \
+    for (p = (head)->next; p != (head); p = p->next)
+
+#define list_for_each_safe(p, n, head) \
+    for (p = (head)->next, n = p->next; p != (head); p = n, n = p->next)
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
