44a48c737f979acd3c2a5917ad927bb8763f16f8
dantin <chengjie.ding@gmail.com>
2019-05-06 17:15:34 +0800

    add some code.

--- leetcode/140-word-break-ii/solution.c
+++ leetcode/140-word-break-ii/solution.c
@@ -14,6 +14,28 @@
 #define list_for_each_safe(p, n, head) \
     for (p = (head)->next, n = p->next; p != (head); p = n, n = p->next)
 
+struct list_head {
+    struct list_head *next, *prev;
+};
+
+void INIT_LIST_HEAD(struct list_head *list)
+{
+    list->next = list->prev = list;
+}
+
+int list_empty(const struct list_head *head)
+{
+    return head->next == head;
+}
+
+void __list_add(struct list_head *new, struct list_head *prev, struct list_head *next)
+{
+    next->prev = new;
+    new->next = next;
+    new->prev = prev;
+    prev->next = new;
+}
+
 int main(int argc, char **argv)
 {
     return EXIT_SUCCESS;
