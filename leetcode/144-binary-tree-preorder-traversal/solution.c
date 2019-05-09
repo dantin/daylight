ade4ce5d245628e3c4249b95f99d85989e3726e1
dantin <chengjie.ding@gmail.com>
2019-05-09 11:51:42 +0800

    solution of binary tree preorder traversal.

---
+++ leetcode/144-binary-tree-preorder-traversal/solution.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int main(int argc, char **argv)
+{
+    return EXIT_SUCCESS;
+}
